b782fdfc0092603b1dea55e2d2cccbdb9efc0966
JesuisJasmin <jasminezeng08@gmail.com>
2022-02-26 13:24:48 +0800

    html updates - pic name & deleted some commented codes

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,6 @@ var elements = document.getElementsByClassName("column");
 // Declare a loop variable
 var i;
 
-// Full-width images
-// function one() {
-//   for (i = 0; i < elements.length; i++) {
-//     elements[i].style.msFlex = "100%";  // IE10
-//     elements[i].style.flex = "100%";
-//   }
-// }
-
 // Two images side by side
 function two() {
   for (i = 0; i < elements.length; i++) {
@@ -29,8 +21,8 @@ function four() {
 }
 
 // Add active class to the current button (highlight it)
-var header = document.getElementById("myHeader");
-var btns = header.getElementsByClassName("btn");
+var header = document.getElementById("gallery-btns");
+var btns = header.getElementsByClassName("btns");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
